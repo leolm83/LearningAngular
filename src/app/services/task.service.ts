6752fef7f2a75d039a674c7952f6e365d4464699
leolm83 <leonardo.mateus01@hotmail.com>
2022-03-20 19:10:19 -0300

    obtendo dados de um servidor

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
-import {Task} from '../interfaces/Task'
-import {TASKS} from '../mocked-data/mocked-tasks'
-import { Observable , of} from 'rxjs';
+import { HttpClient ,HttpHeaders} from '@angular/common/http'
+import { Task } from '../interfaces/Task'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-
-  constructor() { }
+  private apiUrl= 'http://localhost:5000/tasks'
+  constructor(private http:HttpClient) { }
   getTasks(): Observable<Task[]>{
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
